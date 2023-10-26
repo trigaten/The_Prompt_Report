a98870269e639580257df13bbf820258459dc9be
Michael Ilie <mcilieg@gmail.com>
2023-10-26 13:42:21 -0400

    fixed test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 # test_utils.py
 import pytest
-from src.prompt_systematic_review.utils import add, subtract
+from prompt_systematic_review.utils import add, subtract
 
 def test_assert():
     assert True
