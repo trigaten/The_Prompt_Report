e41fe73786793e76b20192917a2b2bca429f1c56
Michael Ilie <mcilieg@gmail.com>
2023-10-26 13:37:12 -0400

    add CI/Testing support

--- src/prompt_systematic_review/utils.py
+++ src/prompt_systematic_review/utils.py
@@ -1,8 +1,8 @@
 import requests
 from xml.etree import ElementTree as ET
 
-def search_arxiv(keyword):
-    url = f"http://export.arxiv.org/api/query?search_query=all:{keyword}&start=0&max_results=10000"
+def search_arxiv(keyword,max_results=10000):
+    url = f"http://export.arxiv.org/api/query?search_query=all:{keyword}&start=0&max_results={max_results}"
     data = requests.get(url).content
     return data
 
---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+# test_utils.py
+import pytest
+from src.prompt_systematic_review.utils import add, subtract
+
+def test_assert():
+    assert True
