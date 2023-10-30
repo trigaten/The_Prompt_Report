9ff75b2e45666fd9a507ee6b5775a4597529ed93
Michael Ilie <mcilieg@gmail.com>
2023-10-30 18:59:20 -0400

    added pipeline and tests II

--- src/prompt_systematic_review/pipeline.py
+++ src/prompt_systematic_review/pipeline.py
@@ -1,27 +1,53 @@
-from datasets import load_dataset, Dataset
+from huggingface_hub import HfFileSystem
+import pandas as pd
 import os
 
 
-def is_authenticated():
-    """
-    Check if the user is authenticated with huggingface-cli.
-    Returns:
-    - bool: True if authenticated, False otherwise.
-    """
-    token_path = os.path.join(os.path.expanduser("~"), ".huggingface", "token")
-    # Check if the token file exists
-    if not os.path.exists(token_path):
-        return False
-    # Check if the token file has content (i.e., the token)
-    with open(token_path, 'r') as f:
-        token = f.read().strip()
-        if not token:
-            return False
+
+
+"""
+READ THIS
+https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions
+https://huggingface.co/docs/huggingface_hub/v0.18.0.rc0/guides/hf_file_system
+
+"""
+
+
+
+
+class Pipeline:
+
+    def __init__(self, token=None):
+        self.token = token
+        self.root = "datasets/PromptSystematicReview/Prompt_Systematic_Review_Dataset/"
+        if token is not None:
+            self.fs = HfFileSystem(token=token)
+        else:
+            self.fs = HfFileSystem()
+
+    def is_logged_in(self):
+        return self.token is not None
     
-    return True
+    def login(self, token):
+        if self.token is not None:
+            raise ValueError("Already Logged In")
+        else:
+            self.fs = HfFileSystem(token=self.token)
+            self.token = token
+
+    def get_all_files(self):
+        return self.fs.ls(self.root, detail=False)
+
+    def get_all_data_files(self):
+        return self.fs.glob(self.root+"**.csv")
+    
+    def read_from_file(self, fileName):
+        return pd.read_csv(self.fs.open(os.path.join(self.root, fileName)))
+    
+    def write_to_file(self, fileName, dataFrame):
+        path = os.path.join(self.root, fileName)
+        self.fs.write_text(path, dataFrame.to_csv(index=False))
 
 
-#make sure
 
-def dummyfunc():
-    return True
+
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,9 +1,43 @@
 # test_pipeline.py
 import pytest
-from prompt_systematic_review.pipeline import is_authenticated
+import os
+from prompt_systematic_review.pipeline import *
+import random
+import time
+import hashlib
 
-def test_is_authenticated():
-    assert is_authenticated() == False
 
-def test_dummy():
-    assert 1 == 1
+def hashString(bytes):
+    return hashlib.sha256(bytes).hexdigest()
+
+@pytest.fixture
+def client():
+    return Pipeline(token=os.environ['HF_TOKEN'])
+
+def test_login():
+    testClient = Pipeline()
+    assert testClient.is_logged_in() == False
+    testClient.login(os.environ['HF_TOKEN'])
+    assert testClient.is_logged_in() == True
+
+def test_get_all_files(client):
+    assert len(client.get_all_files()) > 0
+
+def test_get_all_data_files(client):
+    assert len(client.get_all_data_files()) > 0
+    assert all([x.endswith(".csv") for x in client.get_all_data_files()])
+
+def test_read_from_file(client):
+    assert len(client.read_from_file("test.csv")) > 0
+    assert len(client.read_from_file("test.csv").columns) == 2
+    assert client.read_from_file("test.csv")[" Age"].mean() == 21
+
+def test_write_to_file(client):
+    randString = random.randbytes(100) + str(time.time()).encode()
+    randHash = hashString(randString)
+    csvDict = {"test":[1,3], "test2":[2,4]}
+    client.write_to_file(f"test_{randHash}.csv", pd.DataFrame(csvDict))
+    df = client.read_from_file(f"test_{randHash}.csv") 
+    assert df["test"].sum() == 4
+    assert df["test2"].sum() == 6
+    
