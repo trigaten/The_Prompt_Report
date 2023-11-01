d1e9d8259d36677a20e9786f4199de59837dd11e
trigaten <sanderschulhoff@gmail.com>
2023-10-31 20:42:55 -0400

    fix: remove old arxiv utils

--- src/prompt_systematic_review/utils.py
+++ src/prompt_systematic_review/utils.py
@@ -5,16 +5,3 @@ from xml.etree import ElementTree as ET
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0'
 }
-
-def search_arxiv(keyword,max_results=10000):
-    url = f"http://export.arxiv.org/api/query?search_query=all:{keyword}&start=0&max_results={max_results}"
-    data = requests.get(url).content
-    return data
-
-def count_articles(data):
-    root = ET.fromstring(data)
-    entries = root.findall('{http://www.w3.org/2005/Atom}entry')
-    return len(entries)
-
-
-
