c9f54ed260c72f01f6891c31f280f31eda623d9e
Hudson Tao <hudsontao@gmail.com>
2023-11-01 12:22:35 -0400

    Formatted files with black.

--- src/prompt_systematic_review/utils.py
+++ src/prompt_systematic_review/utils.py
@@ -1,14 +1,14 @@
 import requests
 from xml.etree import ElementTree as ET
 
-def search_arxiv(keyword,max_results=10000):
+
+def search_arxiv(keyword, max_results=10000):
     url = f"http://export.arxiv.org/api/query?search_query=all:{keyword}&start=0&max_results={max_results}"
     data = requests.get(url).content
     return data
 
+
 def count_articles(data):
     root = ET.fromstring(data)
-    entries = root.findall('{http://www.w3.org/2005/Atom}entry')
+    entries = root.findall("{http://www.w3.org/2005/Atom}entry")
     return len(entries)
-
-
