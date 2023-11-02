22539e7cc8c2fdc015867091caca0eabdcde4b9f
trigaten <trigaten@users.noreply.github.com>
2023-11-02 21:52:48 +0000

    deploy: e4129f0fbb951b652a9aa4749596dd2644256e9d

--- tests/test_utils.py
+++
@@ -1,6 +0,0 @@
-# test_utils.py
-from prompt_systematic_review.utils import process_paper_title
-
-
-def test_process_paper_title():
-    assert process_paper_title("Laws  of the\n Wildabeest") == "laws of the wildabeest"
