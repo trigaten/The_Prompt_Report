3b6c1c77e511068d1ae6a641cb7e3e3c37072bfd
Michael Ilie <mcilieg@gmail.com>
2023-11-08 22:51:20 -0500

    quick fix

--- scripts/download_pdfs.py
+++ scripts/download_pdfs.py
@@ -3,6 +3,7 @@ from prompt_systematic_review import arxiv_source
 from multiprocessing import Pool
 import pandas as pd
 import time
+from prompt_systematic_review.download_arxiv_query import queryArchive
 
 """
 Download papers from arxiv and save them to a csv file.
@@ -12,7 +13,10 @@ with a pool of NUM_PROCESSES processes (adjust as needed per your machine).
 """
 
 
-df = pd.read_csv("<filename>.csv")
+filename = "arxiv_papers.csv"
+
+queryArchive(filename)
+df = pd.read_csv(filename)
 # instantiating the arxiv source
 aSource = arxiv_source.ArXivSource()
 
