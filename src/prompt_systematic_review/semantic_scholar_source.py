a3aeb753bb44494c406e20fb6525417ace32d93b
AayushGupta16 <aayugupta04@gmail.com>
2023-11-12 07:58:11 -0500

    first commmit

--- src/prompt_systematic_review/semantic_scholar_source.py
+++ src/prompt_systematic_review/semantic_scholar_source.py
@@ -10,48 +10,35 @@ class SemanticScholarSource:
     searchBaseURL = "https://api.semanticscholar.org/graph/v1/paper/search"
     paperBaseURL = "https://api.semanticscholar.org/graph/v1/paper/"
 
-    def getPapers(
-        self, keyWords: List[str], count: int = 10, offset: int = 0
-    ) -> List[Paper]:
-        """
-        Get a list of papers from Semantic Scholar that match the given keywords.
-        :param keyWords: A list of keywords to match.
-        :param count: The number of papers to retrieve.
-        :param offset: The offset to start retrieving papers from.
-        :return: A list of matching papers.
-        """
-        papers = []
+    def __init__(self):
+        self.api_key = "QsbGjXIFx63q9C49WjKBd5vgNndrIzlb8EkDT3PJ"
+
+    def getPapers(self, keyWords: List[str], count: int = 10, offset: int = 0) -> List[dict]:
+        headers = {"x-api-key": self.api_key}
+        papers_data = []
         for keyword in keyWords:
             params = {
                 "query": keyword,
                 "offset": offset,
                 "limit": count,
-                "fields": "title,authors,year,paperId",
+                "fields": "title,authors,abstract,openAccessPdf,tldr",
             }
-            response = requests.get(self.searchBaseURL, params=params)
+            response = requests.get(self.searchBaseURL, headers=headers, params=params)
             response.raise_for_status()
             data = response.json()
 
             for paper_data in data.get("data", []):
-                title = paper_data.get("title")
-                authors = [
-                    author.get("name") for author in paper_data.get("authors", [])
-                ]
-                year = paper_data.get("year", None)
-                paper_id = paper_data.get("paperId")
-                url = f"https://api.semanticscholar.org/{paper_id}"
+                first_author = paper_data.get("authors", [{}])[0].get("name", "") if paper_data.get("authors") else ""
+                paper_info = {
+                    'Title': paper_data.get("title"),
+                    'First Author': first_author,
+                    'Abstract': paper_data.get('Abstract', '').replace('\n', ' '),
+                    'TLDR': paper_data.get("tldr"),
+                    'Open Access PDF URL': paper_data.get("openAccessPdf", {}).get("url") if paper_data.get("openAccessPdf") else None
+                }
+                papers_data.append(paper_info)
+        return papers_data
 
-                date_submitted = datetime(year, 1, 1).date() if year else None
-
-                paper = Paper(
-                    title=title,
-                    firstAuthor=authors[0] if authors else "",
-                    url=url,
-                    dateSubmitted=date_submitted,
-                    keyWords=[keyword.lower()],
-                )
-                papers.append(paper)
-        return papers
 
     def getPaperDetails(self, paperId: str) -> dict:
         """Get the detailed information of a paper from Semantic Scholar."""
