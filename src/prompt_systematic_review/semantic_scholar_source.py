bb780ef169f32ddc38668376087de6929f4ff12e
AayushGupta16 <aayugupta04@gmail.com>
2023-11-17 19:46:46 -0500

    get_papers working but broken

--- src/prompt_systematic_review/semantic_scholar_source.py
+++ src/prompt_systematic_review/semantic_scholar_source.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime
 from typing import List
 from prompt_systematic_review.paperSource import Paper
+import time
 
 
 class SemanticScholarSource:
@@ -9,102 +10,94 @@ class SemanticScholarSource:
 
     searchBaseURL = "https://api.semanticscholar.org/graph/v1/paper/search"
     paperBaseURL = "https://api.semanticscholar.org/graph/v1/paper/"
-    bulkSearchURL = "https://api.semanticscholar.org/graph/v1/paper/search/bulk"
 
-    # def __init__(self):
-    #     self.session = requests.Session()
-    #     self.session.headers.update({"x-api-key": self.api_key})
-
-    def getPapers(
-        self, keyWords: List[str], count: int = 10, offset: int = 0
-    ) -> List[dict]:
-        papers_data = []
+    def getPapers(self, count: int, keyWords: List[str]) -> List[Paper]:
+        all_papers = []
+        max_retries = 5  # Maximum number of retries after hitting rate limit
         for keyword in keyWords:
-            params = {
-                "query": keyword,
-                "offset": offset,
-                "limit": count,
-                "fields": "title,authors,abstract,openAccessPdf,tldr",
-            }
-            # response = self.session.get(self.searchBaseURL, params=params)
-            response = requests.get(self.searchBaseURL, params=params)
-            response.raise_for_status()
-            data = response.json()
+            papers = []
+            offset = 0
+            retry_count = 0  # Count of current retries
+            while len(papers) < count and retry_count < max_retries:
+                papers_data = []  # Initialize papers_data before the try block
+                try:
+                    papers_data = self.fetchPapersByKeyword(keyword, count - len(papers), offset)
+                    for paper_data in papers_data:
+                        paper = Paper(
+                            title=paper_data['Title'],
+                            firstAuthor=paper_data['First Author'],
+                            url=paper_data.get('Open Access PDF URL'),
+                            dateSubmitted=datetime.strptime(paper_data['Publication Date'], '%Y-%m-%d').date(),
+                            keyWords=[keyword],
+                            abstract=paper_data.get('Abstract')
+                        )
+                        papers.append(paper)
+                        if len(papers) >= count:
+                            break
+                    offset += len(papers_data)
+                    retry_count = 0  # Reset retry count after a successful fetch
+                except requests.exceptions.HTTPError as e:
+                    if e.response.status_code == 429:
+                        print("Rate limit hit. Waiting before retrying...")
+                        time.sleep(1.1 * (retry_count + 1))  # Exponential back-off
+                        retry_count += 1
+                    else:
+                        raise e
+                if len(papers_data) < count:  # Less papers returned than requested
+                    break
+            all_papers.extend(papers)
+        return all_papers
 
-            for paper_data in data.get("data", []):
-                first_author = (
-                    paper_data.get("authors", [{}])[0].get("name", "")
-                    if paper_data.get("authors")
-                    else ""
-                )
-                abstract = (paper_data.get("abstract") or "").replace("\n", " ")
 
-                paper_info = {
-                    "Title": paper_data.get("title"),
-                    "First Author": first_author,
-                    "Abstract": abstract,
-                    "TLDR": paper_data.get("tldr"),
-                    "Open Access PDF URL": paper_data.get("openAccessPdf", {}).get(
-                        "url"
-                    )
-                    if paper_data.get("openAccessPdf")
-                    else None,
-                }
-                papers_data.append(paper_info)
-        return papers_data
 
-    def getPaperDetails(self, paperId: str) -> dict:
-        """Get the detailed information of a paper from Semantic Scholar."""
-        url = f"{self.paperBaseURL}{paperId}?fields=url,openAccessPdf"
-        response = requests.get(url)
+    def fetchPapersByKeyword(self, keyword: str, count: int, offset: int) -> List[dict]:
+        params = {
+            "query": keyword,
+            "offset": offset,
+            "limit": count,
+            "fields": "title,authors,abstract,publicationDate,openAccessPdf"
+        }
+        response = requests.get(self.searchBaseURL, params=params)
         response.raise_for_status()
         data = response.json()
-        return data
 
-    def getPaperPDF(self, paperId: str) -> str:
-        """Get the URL of a paper from Semantic Scholar."""
-        paper_details = self.getPaperDetails(paperId)
-        open_access_pdf_data = paper_details.get("openAccessPdf")
-        if open_access_pdf_data:
-            return open_access_pdf_data.get("url")
-        return None
+        papers_data = []
+        for paper in data.get("data", []):
+            first_author = paper.get("authors", [{}])[0].get("name", "") if paper.get("authors") else ""
+            abstract = paper.get("abstract") or ""
+            publication_date = paper.get("publicationDate") or "1900-01-01"  # Default if not available
 
-    def getOpenAccessPapers(self, keyWords: List[str], n: int) -> List[Paper]:
-        """
-        Get a list of n papers with an open access PDF from Semantic Scholar that match the given keywords.
-        :param keyWords: A list of keywords to match.
-        :param n: The number of papers with an open access PDF to retrieve.
-        :return: A list of papers with an open access PDF.
-        """
-        open_access_papers = []
-        offset = 0
-        while len(open_access_papers) < n:
-            batch = self.getPapers(keyWords, count=100, offset=offset)
-            for paper in batch:
-                if len(open_access_papers) >= n:
-                    break
-                if self.getPaperPDF(paper.url.split("/")[-1]):
-                    open_access_papers.append(paper)
-            offset += len(batch)
-        return open_access_papers
+            paper_info = {
+                "Title": paper.get("title"),
+                "First Author": first_author,
+                "Abstract": abstract,
+                "Publication Date": publication_date,
+                "Open Access PDF URL": paper.get("openAccessPdf", {}).get("url") if paper.get("openAccessPdf") else None
+            }
+            papers_data.append(paper_info)
 
-    def bulkSearchPapers(
-        self,
-        query: str,
-        fields: str = "title,authors,abstract,openAccessPdf,tldr",
-        token: str = None,
-    ) -> dict:
-        params = {
-            "query": query,
-            "fields": fields,
-            "limit": 1000,  # Max limit per API call
-            "sort": "citationCount:desc",  # Example sorting parameter
-        }
-        if token:
-            params["token"] = token
+        return papers_data
 
-        response = requests.get(
-            self.bulkSearchURL, params=params
-        )  # Ensure correct request is made
-        response.raise_for_status()
-        return response.json()
+
+
+# This is assuming you have the SemanticScholarSource class defined as per the previous code
+
+# Create an instance of the SemanticScholarSource class
+semantic_scholar = SemanticScholarSource()
+
+# Define some sample keywords and a count
+keywords = ["deep learning", "Prompt Engineering"]
+count = 5  # Number of papers to fetch for each keyword
+
+# Fetch papers
+papers = semantic_scholar.getPapers(count, keywords)
+
+# Print the details of each paper
+for paper in papers:
+    print("Title:", paper.title)
+    print("First Author:", paper.firstAuthor)
+    print("URL:", paper.url or "Not Available")
+    print("Date Submitted:", paper.dateSubmitted)
+    print("Keywords:", paper.keywords)
+    print("Abstract:", paper.abstract or "No Abstract Available")
+    print("-" * 50)  # Separator for readability
