5e7124d28899ec5f1a14a54d2beefcfa2657191c
trigaten <sanderschulhoff@gmail.com>
2023-11-20 16:11:43 -0500

    style: black (and some edits)

--- scripts/single_use/dedupe_blacklist.py
+++ scripts/single_use/dedupe_blacklist.py
@@ -2,7 +2,7 @@ import pandas as pd
 from prompt_systematic_review.utils import process_paper_title
 
 # Load the CSV file
-df = pd.read_csv('../../data/blacklist.csv')
+df = pd.read_csv("../../data/blacklist.csv")
 
 df["title"] = df["title"].apply(process_paper_title)
 
@@ -10,14 +10,14 @@ df["title"] = df["title"].apply(process_paper_title)
 old_length = len(df)
 
 # Remove duplicates based on 'title' column
-df = df.drop_duplicates(subset='title')
+df = df.drop_duplicates(subset="title")
 
 # Get the number of rows after removing duplicates
 new_length = len(df)
 
 # Print old and new length
-print(f'Old length: {old_length}')
-print(f'New length: {new_length}')
+print(f"Old length: {old_length}")
+print(f"New length: {new_length}")
 
 # Save the DataFrame to the same CSV file
-df.to_csv('../../data/blacklist.csv', index=False)
+df.to_csv("../../data/blacklist.csv", index=False)
--- src/prompt_systematic_review/utils.py
+++ src/prompt_systematic_review/utils.py
@@ -19,4 +19,20 @@ def process_paper_title(title: str) -> str:
     :rtype: str
     """
     # Replace '-' with '', collapse multiple spaces to a single space, and strip leading/trailing spaces
-    return re.sub(r"\s+", " ", title.lower().replace("\n", "").replace("-", "")).strip()
+    s = (
+        re.sub(r"\s+", " ", title.lower().replace("\n", "").replace("-", ""))
+        .strip()
+        .replace(".", "")
+        .replace("/", "")
+        .replace("\\", "")
+        .replace(":", "")
+        .replace("*", "")
+        .replace("?", "")
+        .replace('"', "")
+        .replace("<", "")
+        .replace(">", "")
+        .replace("|", "")
+    )
+    # .replace(" ", "")
+    # s = re.sub(r'\W+', '', s)
+    return s
