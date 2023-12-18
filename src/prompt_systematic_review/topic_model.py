963187481c28804b06054bb4588652c9b7703cb7
Hudson Tao <hudsontao@gmail.com>
2023-12-18 00:34:20 -0500

    feat: mitigated topic_model directory dependency

--- src/prompt_systematic_review/topic_model.py
+++ src/prompt_systematic_review/topic_model.py
@@ -1,15 +1,29 @@
 import subprocess
 import os
 import pandas as pd
+from importlib import resources as rsc
 
 
 """
-Runs the topic model with default parameters used in data/topic-model-data/README.md (MANUAL section). 
-Results are found in topic-model-data folder
+Runs the topic model with default parameters seen in data/topic-model-data/README.md (MANUAL section). 
+Results are found in topic-model-data/topic-model-outputs folder
 Runs all topic models (10, 25, 50). If you don't want to run all of them, feel free to comment out below.
 
-Relies on the user being in ./Prompt_Systematic_Review/src/prompt_systematic_review
 """
+
+"""Summary: Changes directory to /data
+"""
+def change_dir_data():
+    DIRNAME = os.path.split(__file__)[0]
+    BACK = os.sep + os.pardir
+    os.chdir(os.path.normpath(DIRNAME + BACK + BACK))
+    os.chdir(os.path.join(os.getcwd(), "data"))
+
+"""Summary: Changes directory to /src/prompt_systematic_review
+"""
+def change_dir_src():
+    DIRNAME = os.path.split(__file__)[0]
+    os.chdir(DIRNAME)
 
 
 """Summary: Downloads master_papers.csv from PromptSystematicReview huggingface database to topic-model folder
@@ -95,12 +109,16 @@ def run_preprocess():
 """
 
 
-def run_topic_model():
+def run():
+    change_dir_data()
+
     get_csv()
 
     run_detect_phrases()
 
     run_preprocess()
+
+    change_dir_src()
 
     # 10 topics
     subprocess.run(
